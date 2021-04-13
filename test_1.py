4d9fc03a57d9dcdfd9fa71a28832bec05d08729f
Andres Martinez <andres_m559@Andress-MBP.attlocal.net>
2021-04-13 09:59:32 -0700

    finish pseudocode

--- test_1.py
+++ test_1.py
@@ -1,6 +1,5 @@
 
 
-
 """
 The program is trying to determine which payment option is better (more money).
 First option is 100 dollars per day for 10 days. The second option is 1 dollar 
@@ -13,4 +12,32 @@ function2 will loop 10 times, with each time doubling the amount and adding the 
 if the amount is equal, we output to the user "Option 1 and Option 2 pays the same"
 if option1 is better, we output to the user "Option 1 is better"
 if option2 is better, we output to the user "Option 2 is better"
+"""
+
+""""
+
+# option1
+    return 100 * 10
+
+# option2
+    amount = 1
+    list1 = []
+    loop 10 times
+        add amount to list1
+        amount *=2
+    sum = sum of all items in loop
+    return sum
+
+# main
+    var1 = option1
+    var2 = option2
+
+    if var1 = var2  
+        "Option 1 and Option 2 pays the same"
+    if var1 < var2
+        "Option 2 is better"
+    else 
+        "Option 1 is better"    
+
+main
 """
